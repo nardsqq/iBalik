9a51c74ebe87e7d2da6e2d0d286607ba9e7ba5fd
nardsqq <nardparagas@gmail.com>
2017-05-01 22:56:37 +0800

    Added _partials for readability and structure

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,6 @@
+<div class="col-md-8 col-md-offset-2">
+	<hr>
+	<div class="text-center">
+		<p>iBalik 2017 - All Rights Reserved</p>
+    </div>
+</div>
---
+++ resources/views/partials/_script.blade.php
@@ -0,0 +1,5 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="{{ asset('/js/jquery/jquery-3.1.1.min.js') }}"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="{{ asset('/js/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/js/DataTables/datatables.min.js') }}"></script>
