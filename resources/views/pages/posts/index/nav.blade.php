a918af14798bbe00ebf3a1ff0ef044667da0db29
nardsqq <nardparagas@gmail.com>
2017-06-29 03:43:20 +0800

    Updated Navigation

--- resources/views/pages/posts/index/nav.blade.php
+++ resources/views/pages/posts/index/nav.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">iBalik</a>
+      <a class="navbar-brand" href="{{ url('/') }}">iBalik</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
